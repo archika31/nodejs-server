dbe3353ba37e7a6b8cdac30659b1cd8be837ea4b
Kasliwal Jain <archika.kasliwal-jain@capgemini.com>
2025-05-14 10:47:46 +0530

    change1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT=3000;
 const server=http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Hello World! \n');
+    res.end('Hello World Archika! \n');
 });
 server.listen(PORT,()=>{
     console.log(`Server running at http://localhost:${PORT}/`);
